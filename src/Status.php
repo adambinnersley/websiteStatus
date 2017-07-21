50dc6716107a76ea6f5a0e5f5da6d7e5a391de00
Adam Binnersley <abinnersley@gmail.com>
2017-07-21 14:45:12 +0100

    Bug fix

--- src/Status.php
+++ src/Status.php
@@ -74,7 +74,8 @@ class Status{
     }
     
     protected function getSSLCert($url){
-        $orignal_parse = parse_url($url, PHP_URL_HOST);
+        $domain = 'https://'.str_replace(array('http://', 'https://'), '', strtolower($url)); // Force it to look at the https otherwise it fails
+        $orignal_parse = parse_url($domain, PHP_URL_HOST);
         $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
         $read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
         $cert = stream_context_get_params($read);
