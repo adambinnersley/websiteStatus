d55adfba66e98ae6ac36ec188f8379c0a8e1e735
Adam Binnersley <abinnersley@gmail.com>
2017-08-04 11:33:25 +0100

    Remove unnecessary code

--- database/site_status.sql
+++ database/site_status.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jul 21, 2017 at 11:14 AM
--- Server version: 5.7.9
--- PHP Version: 7.1.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `instructor_websites`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `site_status`
 --
@@ -34,7 +10,3 @@ CREATE TABLE IF NOT EXISTS `site_status` (
   PRIMARY KEY (`website`),
   UNIQUE KEY `website` (`website`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
