68606de7f6568c9161d9a2f37564eb5593fc7ad8
Adam Binnersley <1990042+adambinnersley@users.noreply.github.com>
2024-09-19 11:38:57 +0100

    Update site_status.sql

--- database/site_status.sql
+++ database/site_status.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `site_status` (
   `website` varchar(255) NOT NULL,
   `status` mediumint(3) UNSIGNED NOT NULL,
   `ssl_expiry` datetime DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
   PRIMARY KEY (`website`),
   UNIQUE KEY `website` (`website`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
